ffb0b15e42690df372942f952e42453ef9b59a02
Sahar Soheilian <sahar.soheilian@gmail.com>
2021-06-07 00:28:49 +0000

    initial test unit implemetation for testing LambdaStreamImp

---
+++ core_java/grep/src/test/java/ca/jrvs/apps/practice/LambdaStreamImpTest.java
@@ -0,0 +1,55 @@
+package ca.jrvs.apps.practice;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.BeforeEach;
+
+// TODO: implement it after learning junit
+public class LambdaStreamImpTest extends TestCase {
+
+  @BeforeEach
+  public void setUp() {
+    LambdaStreamImp lambdaStream = new LambdaStreamImp();
+  }
+
+  public void testCreateStrStream() {
+  }
+
+  public void testToUpperCase() {
+  }
+
+  public void testFilter() {
+  }
+
+  public void testCreateIntStream() {
+  }
+
+  public void testToList() {
+  }
+
+  public void testTestToList() {
+  }
+
+  public void testTestCreateIntStream() {
+  }
+
+  public void testSquareRootIntStream() {
+  }
+
+  public void testGetOdd() {
+  }
+
+  public void testGetLambdaPrinter() {
+  }
+
+  public void testPrintMessages() {
+  }
+
+  public void testPrintOdd() {
+  }
+
+  public void testFlatNestedInt() {
+  }
+
+  public void testMain() {
+  }
+}
