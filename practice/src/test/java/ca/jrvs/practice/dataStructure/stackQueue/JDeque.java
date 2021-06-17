0f0efc9df40e38abac3e6d6d5eea78c08ae83b01
Sahar Soheilian <sahar.soheilian@gmail.com>
2021-06-17 03:38:43 +0000

    implement JDeque(JQueue and JStack)

--- practice/src/test/java/ca/jrvs/practice/dataStructure/stackQueue/JDeque.java
+++ practice/src/test/java/ca/jrvs/practice/dataStructure/stackQueue/JDeque.java
@@ -1,5 +1,9 @@
 package ca.jrvs.practice.dataStructure.stackQueue;
 
-public interface JDeque {
+/**
+ * This is a simplified version of java.util.Deque
+ *
+ */
+public interface JDeque<E> extends JStack<E>, JQueue<E> {
 
 }
--- practice/src/test/java/ca/jrvs/practice/dataStructure/stackQueue/LinkedJListJDeque.java
+++ practice/src/test/java/ca/jrvs/practice/dataStructure/stackQueue/LinkedJListJDeque.java
@@ -1,2 +1,95 @@
-package ca.jrvs.practice.dataStructure.stackQueue;public class LinkedJListJDeque<E> implements JDeque<E> {
+package ca.jrvs.practice.dataStructure.stackQueue;
+
+import ca.jrvs.practice.dataStructure.list.LinkedJList;
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+public class LinkedJListJDeque<E> implements JDeque<E> {
+
+  LinkedJList<E> linkedJList;
+
+  /**
+   * This is equivalent enqueue operation in Queue ADT
+   * <p>
+   * Inserts the specified element into the queue represented by this deque (in other words, at the
+   * tail of this deque) if it is possible to do so immediately without violating capacity
+   * restrictions, returning {@code true} upon success and throwing an {@code IllegalStateException}
+   * if no space is currently available.
+   *
+   * @param e the element to add
+   * @return {@code true} (as specified by {@link Collection#add})
+   * @throws NullPointerException if the specified element is null and this deque does not permit
+   *                              null elements
+   */
+  @Override
+  public boolean add(E e) {
+    if (e == null)
+      throw new NullPointerException("Null elements are not allowed");
+
+    return linkedJList.add(e);
+  }
+
+  /**
+   * This is equivalent dequeue operation in Queue ADT
+   * <p>
+   * Retrieves and removes the head of the queue represented by this deque (in other words, the
+   * first element of this deque).
+   *
+   * @return the head of the queue represented by this deque
+   * @throws NoSuchElementException if this deque is empty
+   */
+  @Override
+  public E remove() {
+    if (linkedJList.size() == 0)
+      throw new NoSuchElementException("Empty deque: No element to remove");
+
+    return linkedJList.remove(0);
+  }
+
+  /**
+   * Pops an element from the stack represented by this deque. In other words, removes and returns
+   * the first element of this deque.
+   *
+   * @return the element at the front of this deque (which is the top of the stack represented by
+   * this deque)
+   * @throws NoSuchElementException if this deque is empty
+   */
+  @Override
+  public E pop() {
+    if (linkedJList.isEmpty())
+      throw new NoSuchElementException("Empty deque: No element to pop");
+
+    return linkedJList.remove(0);
+  }
+
+  /**
+   * Pushes an element onto the stack represented by this deque (in other words, at the head of this
+   * deque) if it is possible to do so immediately without violating capacity restrictions
+   *
+   * @param e the element to push
+   * @throws NullPointerException if the specified element is null and this deque does not permit
+   *                              null elements
+   */
+  @Override
+  public void push(E e) {
+    if (e == null)
+      throw new NullPointerException("Null elements are not allowed");
+
+    linkedJList.add(0, e);
+
+  }
+
+  /**
+   * Retrieves, but does not remove, the head of the queue represented by this deque (in other
+   * words, the first element of this deque), or returns {@code null} if this deque is empty.
+   *
+   * @return the head of the queue represented by this deque, or {@code null} if this deque is empty
+   */
+  @Override
+  public E peek() {
+    if (linkedJList.isEmpty())
+      return null;
+
+    return linkedJList.get(0);
+  }
 }
